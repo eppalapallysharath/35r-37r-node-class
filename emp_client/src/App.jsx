011e6ce4360d90e109c7ff3609c9a09e9c6c115d
eppalapallysharath <eppalapallysharath@gmail.com>
2025-09-11 17:38:36 +0530

    updated url of backend

--- emp_client/src/App.jsx
+++ emp_client/src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 
+const baseUrl = "https://employee-management-api-u9vq.onrender.com"
+// const baseUrl = "http://localhost:3000"
+
 const App = () => {
   const [name, setName] = useState("");
   const [role, setRole] = useState("");
   const [salary, setSalary] = useState("");
   useEffect(() => {
-    fetch("http://localhost:3000/getallemployees")
+    fetch(`${baseUrl}/getallemployees`)
       .then((res) => res.json())
       .then((data) => console.log(data))
       .catch((err) => console.log(err));
@@ -16,7 +19,7 @@ const App = () => {
 
     const inputdata = { name: name, salary: salary, role: role }
 
-    fetch("http://localhost:3000/createEmployee", {
+    fetch(`${baseUrl}/createEmployee`, {
       method: "POST",
       headers: {
         "Content-Type":"application/json"
